a3030049775f51418d3337d0bb36d13e8991efdb
AmrendraCodes <amrendra1999official@gmail.com>
2026-01-06 09:36:25 +0530

    file commited

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,11 +1,3 @@
-import { useState, useMemo, useEffect } from "react";
-import { Search, Home, Users, Mail, Shield, FileText } from "lucide-react";
-import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
-import { Link } from "react-router-dom";
-import Layout from "../components/Layout";
-import { LazyBlogCard } from "../components/LazyBlogCard";
-import { fetchArticles, mapArticlesToUi } from "../lib/strapi";
 import ArticlesList from '../components/ArticleList';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
