648bebd05350afb1524a6765340537e4a2247326
Amrendra Kumar <Amrendra1999official@gmail.com>
2026-01-09 15:03:07 +0530

    File commited
    Updated ArticlesList component to display a list of articles with titles and descriptions.

--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -1,8 +1,18 @@
 export const ArticlesList = () => {
+  const articles = [
+    { id: 1, title: 'First Article', description: 'This is my first article' },
+    { id: 2, title: 'Second Article', description: 'This is my second article' },
+  ]
+
   return (
     <div className="articles-list">
-      <h2>Articles</h2>
-      <p>Articles will be displayed here</p>
+      <h2>My Articles</h2>
+      {articles.map(article => (
+        <article key={article.id}>
+          <h3>{article.title}</h3>
+          <p>{article.description}</p>
+        </article>
+      ))}
     </div>
   )
 }
